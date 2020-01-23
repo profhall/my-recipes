527786ef8bee95894cb47a86981720cc82307480
Phillip Hall <phillip_hall@homedepot.com>
2020-01-23 16:00:02 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import app from "./fbase"
+import {navigate} from "hookrouter";
+import * as firebase from "firebase/app";
+import 'firebase/firestore';
 
 function App() {
+
+  useEffect(()=>{
+    async function getFBase() {
+      const db = await firebase.firestore(app);
+      let mainsDB = await db.collection(`mains`).get();
+      console.log(mainsDB.docs.map(doc => doc.data()))
+    }
+    getFBase();
+
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
